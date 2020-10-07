3ca2dc0bb045742f03161771053e0e1840e2e8b4
Your Github User Name <david.lav.williams@gmail.com>
2020-10-07 14:26:34 -0500

    I got the location to autofill with java

---
+++ scripts/LocationList.js
@@ -0,0 +1,34 @@
+// This will need to import data and tips
+
+import { getLocation } from "./LocationDataProvider.js";
+import { createLocation } from "./Locations.js";
+
+// this will need to create a function which 1) defines the selector
+export function locationList() {
+    // defines the selector area to place the text
+
+    const locationArea = document.querySelector(".Travel__locations_flex");
+    // gets data from provider
+
+    const locations = getLocation()
+    //  create a string with the data from tips
+
+    let locationHTML = " "
+    // create a loop which will go through the list
+
+    for (const butthole of locations) {
+        locationHTML += createLocation(butthole)
+
+    }
+    // this part puts in where the text needs to go
+
+    console.log(locationHTML)
+    locationArea.innerHTML += `
+    
+    
+        ${locationHTML}
+    
+    `
+
+}
+
