be65867e6e0d6d661b1b58ffb1ac141dba8fefc0
Your Github User Name <david.lav.williams@gmail.com>
2020-10-07 09:03:23 -0500

    adding FishDataProvider trial 1

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,23 @@
+const fishCollection =[
+    {
+        Species: "Scorpaenopsis oxycephala",
+        Name: "Flub",
+        Length: "9 inches",
+        Location: "Indian Ocean",
+        Food: "Crustaceans and Smaller Fish"
+    },
+    {
+        Species: "Platax scalaris",
+        Name: "Bubbles",
+        Length: "2 inches",
+        Location: "Amazon River",
+        Food: "Bugs and small fish"
+    },
+    {
+        Species: "Paracanthurus hepatus",
+        Name: "Dory",
+        Length: "9-12inch",
+        Location: "IndoPacific",
+        Food: "Plankton"
+    },
+]
